5c00439786d32c5b578ea04f9fad3ab392e0b368
NikhilSinghRathod <r7445580@gmail.com>
2025-01-08 21:45:34 +0530

    Average of three number program

---
+++ Averageofthree.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Averageofthree{
+  public static void main(String[]args){
+   Scanner sc = new Scanner(System.in);
+     System.out.print("Enter the Number :");
+     int x = sc.nextInt();
+     System.out.print("Enter the Number :");
+     int y = sc.nextInt();
+     System.out.print("Enter the Number :");
+     int z= sc.nextInt();
+     int n = (x+y+z)/3;
+     System.out.print(n);
+    }
+   }
